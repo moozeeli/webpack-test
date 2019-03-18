3adf7cdd5c07d2e57534422da8ce765e4fe61daf
liton <921006756@qq.com>
2019-03-18 12:43:20 +0800

    稍作修改，添加readme.mk
    Signed-off-by: liton <921006756@qq.com>

---
+++ demo-outputTest/readme.md
@@ -0,0 +1,12 @@
+### demo-output 目录说明
+
+此目录下是 webpack 输出管理指南的实现测试。
+指南文档：https://www.webpackjs.com
+2019年3月18日
+
+
+##### 管理输出
+
+管理输出，主要是使用 html-webpack-plugin 自动生成 html；
+
+1
--- demo-outputTest/src/index.js
+++ demo-outputTest/src/index.js
@@ -2,9 +2,9 @@ import "./style.css";
 import Data from "./data.json";
 
 var body = document.querySelector("body");
-body.append("index.js->bundle.js<br>");
-
-
+body.append("自动编译，但不会热加载");
+body.append(document.createElement("br"));
+body.append(document.createElement("hr"));
 let dataString = JSON.stringify(Data);
 console.log(Data)
 body.append(dataString);
