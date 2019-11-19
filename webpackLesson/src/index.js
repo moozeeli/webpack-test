0ede0f23724fa46653bca3a884a398dbe3ad47f2
liton <921006756@qq.com>
2019-11-19 09:27:23 +0800

    添加 webpaackLesson 项目目录
    Signed-off-by: liton <921006756@qq.com>

---
+++ webpackLesson/src/index.js
@@ -0,0 +1,7 @@
+﻿import "./style/index.css";
+let root = document.getElementById("root");
+
+let div = document.createElement("div");
+div.innerHTML = "<p class='p'> 这是个段落</p>"
+
+root.appendChild(div);
---
+++ webpackLesson/webpack.config.js
@@ -0,0 +1,33 @@
+﻿const path = require("path");
+const webpack = require("webpack");
+const {CleanWebpackPlugin} = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+    entry:"./src/index.js",
+    output:{
+        filename:"[name][hash:5].js",
+        path: path.resolve(__dirname,'dist')
+    },
+    devServer:{
+        contentBase:"./dist",
+        open:true,
+        hot:true, //开启HMR
+    },
+    mode:'development',
+    module:{
+        rules:[
+            {
+                test:/.css$/,
+                loader:["style-loader","css-loader"]
+            }
+        ]
+    },
+    plugins:[
+        new webpack.HotModuleReplacementPlugin(),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template:"./src/index.html"
+        })
+        
+    ]
+}
