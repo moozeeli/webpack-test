d65cd1cd771e74c2ac91c3ec3b9dcd7388232cce
liton <921006756@qq.com>
2019-11-27 21:24:08 +0800

    测试模块语法
    Signed-off-by: liton <921006756@qq.com>

--- webpackLesson/src/index.js
+++ webpackLesson/src/index.js
@@ -8,18 +8,47 @@ import iconfont from "./style/iconfont/iconfont.css";
 import { Header } from "./components/Header/index";
 import { Bottom } from "./components/Bottom/index";
 
-import paiTest, { PAI, PAI2, PAI3, p1, p2, p3 } from "./moduleTest/PAI";
-import PAIdefault, * as paiTest2 from "./moduleTest/PAI";
+import { PAI, PAI2, PAI3, p1, p2, p3 } from "./moduleTest/PAI";
+import PAIdefault, * as PAI_Module from "./moduleTest/PAI";
+import commonjsModule from "./moduleTest/commonjsModule.js";
+import {name} from "./moduleTest/commonjsModule.js";
 
-console.log("paiTest2", paiTest2);
+console.log("=======================");
+console.table(PAI, PAI2,PAI3);
+console.log("=======================");
+console.log("PAIdefault");
+console.table(PAIdefault)
+
+console.log("=======================");
+console.log("commonjsModule",commonjsModule);
+console.log("修改 commonjsModule 属性");
+try {
+    commonjsModule.age = 100; // 可正常修改
+    console.log("commonjsModule", commonjsModule);
+} catch (error) {
+    console.error("修改 commonjsModule 属性 报错");        
+}
+console.log("commonjsModule name", name);
+console.log("=======================");
+
 console.log("PAIdefault", PAIdefault);
 console.log(p1,p2,p3);
 
-console.log(PAI);
-console.log(PAI2);
-console.log(PAI3);
-console.log(paiTest);
 
+console.log("修改 PAIdefault");
+PAIdefault.PAI = 12123; // 这里会正常执行，因为PAIdefault 是模块中自己导出的对象，未经冻结处理
+console.log("PAIdefault", PAIdefault);
+
+
+// 测试修改 es6模块能否修改属性值
+console.log("PAI_Module", PAI_Module);
+console.log("修改 PAI_Module");
+try {
+    PAI_Module.PAI = "123123" // 这里会报错    
+} catch (error) {
+    console.error("修改 PAI_Module.PAI 出现错误，因为是ES6 模块语法");
+}
+console.log("PAI_Module", PAI_Module);
 
 let header = new Header();
 if (header) {
---
+++ webpackLesson/src/moduleTest/commonjsModule.js
@@ -0,0 +1,1 @@
+module.exports = { name: 'commonjsModuleName',age:"20" };
